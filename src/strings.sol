2e8987612c1cf6beed5b07227dfce3fbdfa1b54e
piggona <31783726+piggona@users.noreply.github.com>
2019-06-30 19:47:56 +0800

    Update strings.sol

--- src/strings.sol
+++ src/strings.sol
@@ -583,13 +583,8 @@ library strings {
         uint ptr = findPtr(self._len, self._ptr, needle._len, needle._ptr);
         token._ptr = self._ptr;
         token._len = ptr - self._ptr;
-        if (ptr == self._ptr + self._len) {
-            // Not found
-            self._len = 0;
-        } else {
-            self._len -= token._len + needle._len;
-            self._ptr = ptr + needle._len;
-        }
+        self._len -= token._len + needle._len;
+        self._ptr = ptr + needle._len;
         return token;
     }
 
